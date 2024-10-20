ea5487564fafcc70653ae25feec7d9b86c19ff96
Hien Nguyen <hiennc@biso24.com>
2024-10-20 11:45:57 +0700

    Upgrade package

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import SERVICE_BROKER from './service.constant.json'
+import SERVICE_BROKER from "./service.constant.json";
 
-export default SERVICE_BROKER
+export default SERVICE_BROKER;
 
-export type ServiceBrokerContantType = { [key: string]: any }
+export type ServiceBrokerConstantType = { [key: string]: any };
 
-export const addLocalServices = (services: ServiceBrokerContantType) => {
-    Object.assign(SERVICE_BROKER, services)
-    console.log('added local services!')
-}
+export const addLocalServices = (services: ServiceBrokerConstantType) => {
+  Object.assign(SERVICE_BROKER, services);
+  console.log("added local services!");
+};
