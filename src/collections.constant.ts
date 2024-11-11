6415e7489b65b156a627c08f11c3c01ac51fd94c
binhdt <binhdt@biso24.com>
2024-11-11 11:29:34 +0700

    FEAT: add collection constant

---
+++ src/collections.constant.ts
@@ -0,0 +1,406 @@
+// ADMINISTRATION LIST SERVICE
+// Menu
+export const MENU_REPOSITORY_NAME = 'menu';
+export const MENU_COLLECTION_NAME = 'menus';
+
+// Menu Application
+export const MENU_APPLICATION_REPOSITORY_NAME = 'menuApplication';
+export const MENU_APPLICATION_COLLECTION_NAME = 'menu_applications';
+
+// Facility List
+export const FACILITY_LIST_REPOSITORY_NAME = 'facilityList';
+export const FACILITY_LIST_COLLECTION_NAME = 'facility_lists';
+
+// Channel List
+export const CHANNEL_LIST_REPOSITORY_NAME = 'channelList';
+export const CHANNEL_LIST_COLLECTION_NAME = 'channel_lists';
+
+// Currency List
+export const CURRENCY_LIST_REPOSITORY_NAME = 'currencyList';
+export const CURRENCY_LIST_COLLECTION_NAME = 'currency_lists';
+
+// Contract
+export const CONTRACT_LISTS_REPOSITORY_NAME = 'contractList';
+export const CONTRACT_LISTS_COLLECTION_NAME = 'contract_lists';
+export const CONTRACT_COLUMNS_REPOSITORY_NAME = 'contractColumn';
+export const CONTRACT_COLUMNS_COLLECTION_NAME = 'contract_columns';
+export const CONTRACT_MISSIONS_REPOSITORY_NAME = 'contractMission';
+export const CONTRACT_MISSIONS_COLLECTION_NAME = 'contract_missions';
+export const CONTRACT_HISTORIES_REPOSITORY_NAME = 'contractHistory';
+export const CONTRACT_HISTORIES_COLLECTION_NAME = 'contract_histories';
+export const CONTRACT_MISSION_COLUMNS_REPOSITORY_NAME = 'contractMissionColumn';
+export const CONTRACT_MISSION_COLUMNS_COLLECTION_NAME =
+  'contract_mission_columns';
+export const CONTRACT_LIBRARIES_REPOSITORY_NAME = 'contractLibrary';
+export const CONTRACT_LIBRARIES_COLLECTION_NAME = 'contract_libraries';
+export const CONTRACT_KEYS_REPOSITORY_NAME = 'contractKey';
+export const CONTRACT_KEYS_COLLECTION_NAME = 'contract_keys';
+
+// Selling Method
+export const SELLING_METHOD_REPOSITORY_NAME = 'sellingMethod';
+export const SELLING_METHOD_COLLECTION_NAME = 'selling_methods';
+
+// Warehouse
+export const WAREHOUSE_LIST_REPOSITORY_NAME = 'warehouseList';
+export const WAREHOUSE_LIST_COLLECTION_NAME = 'warehouse_lists';
+export const WAREHOUSE_TYPE_REPOSITORY_NAME = 'warehouseType';
+export const WAREHOUSE_TYPE_COLLECTION_NAME = 'warehouse_types';
+
+// Management Criteria
+export const MANAGEMENT_CRITERIA_REPOSITORY_NAME = 'managementCriteria';
+export const MANAGEMENT_CRITERIA_COLLECTION_NAME = 'management_criterias';
+
+// Method List
+export const METHOD_LIST_REPOSITORY_NAME = 'methodList';
+export const METHOD_LIST_COLLECTION_NAME = 'method_lists';
+
+// Method Item
+export const METHOD_ITEM_REPOSITORY_NAME = 'methodItem';
+export const METHOD_ITEM_COLLECTION_NAME = 'method_items';
+
+// Administration List
+export const ADMINISTRATION_LIST_REPOSITORY_NAME = 'administrationList';
+export const ADMINISTRATION_LIST_COLLECTION_NAME = 'administration_lists';
+
+// Administration Item
+export const ADMINISTRATION_ITEMS_REPOSITORY_NAME = 'administrationItem';
+export const ADMINISTRATION_ITEMS_COLLECTION_NAME = 'administration_items';
+
+// Customer
+export const CUSTOMER_REPOSITORY_NAME = 'customer';
+export const CUSTOMER_COLLECTION_NAME = 'customers';
+
+// Customer Detail
+export const CUSTOMER_DETAIL_REPOSITORY_NAME = 'customerDetail';
+export const CUSTOMER_DETAIL_COLLECTION_NAME = 'customer_details';
+
+// ORGANIZATION SERVICE
+// Employee
+export const EMPLOYEE_REPOSITORY_NAME = 'employee';
+export const EMPLOYEE_COLLECTION_NAME = 'employees';
+export const EMPLOYEE_DETAIL_REPOSITORY_NAME = 'employeeDetail';
+export const EMPLOYEE_DETAIL_COLLECTION_NAME = 'employee_details';
+export const EMPLOYEE_SETTING_REPOSITORY_NAME = 'employeeSetting';
+export const EMPLOYEE_SETTING_COLLECTION_NAME = 'employee_settings';
+export const EMPLOYEE_JOB_REPOSITORY_NAME = 'employeeJob';
+export const EMPLOYEE_JOB_COLLECTION_NAME = 'employee_jobs';
+export const EMPLOYEE_SALARY_REPOSITORY_NAME = 'employeeSalary';
+export const EMPLOYEE_SALARY_COLLECTION_NAME = 'employee_salaries';
+export const EMPLOYEE_SALARY_HISTORY_REPOSITORY_NAME = 'employeeSalaryHistory';
+export const EMPLOYEE_SALARY_HISTORY_COLLECTION_NAME =
+  'employee_salary_histories';
+
+// Organization
+export const ORGANIZATION_REPOSITORY_NAME = 'organization';
+export const ORGANIZATION_COLLECTION_NAME = 'organizations';
+
+// Department
+export const DEPARTMENT_REPOSITORY_NAME = 'department';
+export const DEPARTMENT_COLLECTION_NAME = 'departments';
+
+// Position
+export const POSITION_REPOSITORY_NAME = 'position';
+export const POSITION_COLLECTION_NAME = 'positions';
+
+// Org Setting
+export const ORG_SETTING_REPOSITORY_NAME = 'orgSetting';
+export const ORG_SETTING_COLLECTION_NAME = 'org_settings';
+
+// AUTH SERVICE
+// Menu
+export const MENU_BIZCORE_REPOSITORY_NAME = 'bizcoreMenu';
+export const MENU_BIZCORE_COLLECTION_NAME = 'bizcore_menus';
+
+// User
+export const TENANT_USER_REPOSITORY_NAME = 'tenantUser';
+export const TENANT_USER_COLLECTION_NAME = 'tenant_users';
+
+// Application
+export const TENANT_APPLICATION_REPOSITORY_NAME = 'tenantApplication';
+export const TENANT_APPLICATION_COLLECTION_NAME = 'tenant_applications';
+
+// Verify Email Token
+export const VERIFY_EMAIL_TOKEN_REPOSITORY_NAME = 'verifyEmailToken';
+export const VERIFY_EMAIL_TOKEN_COLLECTION_NAME = 'verify_email_tokens';
+
+// Value Chains
+export const VALUE_CHAIN_REPOSITORY_NAME = 'valueChain';
+export const VALUE_CHAIN_COLLECTION_NAME = 'value_chains';
+export const VALUE_CHAIN_TODO_LIST_REPOSITORY_NAME = 'valueChainTodoList';
+export const VALUE_CHAIN_TODO_LIST_COLLECTION_NAME = 'value_chain_todo_lists';
+export const VALUE_CHAIN_SEGMENT_LIST_REPOSITORY_NAME = 'valueChainSegmentList';
+export const VALUE_CHAIN_SEGMENT_LIST_COLLECTION_NAME =
+  'value_chain_segment_lists';
+export const VALUE_CHAIN_PROCESS_LIST_REPOSITORY_NAME = 'valueChainProcessList';
+export const VALUE_CHAIN_PROCESS_LIST_COLLECTION_NAME =
+  'value_chain_process_lists';
+
+// Authentication
+export const REFRESH_TOKEN_REPOSITORY_NAME = 'refreshToken';
+export const REFRESH_TOKEN_COLLECTION_NAME = 'refresh_tokens';
+
+// Authorize
+export const TENANT_ROLE_REPOSITORY_NAME = 'tenantRole';
+export const TENANT_ROLE_COLLECTION_NAME = 'tenant_roles';
+export const APPLICATION_ACTION_REPOSITORY_NAME = 'applicationAction';
+export const APPLICATION_ACTION_COLLECTION_NAME = 'application_actions';
+
+// AUTH2 PLATFORM
+export const AUTH2_REPOSITORY_NAME = 'oAuth2';
+export const AUTH2_COLLECTION_NAME = 'oauth2';
+
+// platform
+export const PLATFORM_REPOSITORY_NAME = 'platformIntegration';
+export const PLATFORM_COLLECTION_NAME = 'platform_integrations';
+
+// Verify Email Token
+export const VERIFY_PLATFORM_TOKEN_REPOSITORY_NAME = 'verifyPlatformToken';
+export const VERIFY_PLATFORM_TOKEN_COLLECTION_NAME = 'verify_platform_tokens';
+
+// Lock Login
+export const LOCK_LOGIN_REPOSITORY_NAME = 'lockLogIn';
+export const LOCK_LOGIN_COLLECTION_NAME = 'lock_log_in';
+
+// SALES PLAN
+// Financial Model
+export const FINANCE_MODAL_LIST_REPOSITORY_NAME = 'financialModelList';
+export const FINANCE_MODAL_LIST_COLLECTION_NAME = 'financial_model_lists';
+export const FINANCE_MODAL_TARGET_REPOSITORY_NAME = 'financialModelTarget';
+export const FINANCE_MODAL_TARGET_COLLECTION_NAME = 'financial_model_targets';
+export const FINANCE_MODAL_CONFIGURATION_REPOSITORY_NAME =
+  'financialModelConfiguration';
+export const FINANCE_MODAL_CONFIGURATION_COLLECTION_NAME =
+  'financial_model_configurations';
+export const FINANCE_MODAL_TARGET_CONFIGURATION_REPOSITORY_NAME =
+  'financialModelTargetConfiguration';
+export const FINANCE_MODAL_TARGET_CONFIGURATION_COLLECTION_NAME =
+  'financial_model_target_configurations';
+
+// Expense List
+export const EXPENSE_LISTS_REPOSITORY_NAME = 'expenseList';
+export const EXPENSE_LISTS_COLLECTION_NAME = 'expense_lists';
+
+// Sales Plan
+export const SALES_PLAN_CORE_COLLECTION_NAME = 'sales_plan_cores';
+export const SALES_PLAN_YEAR_REPOSITORY_NAME = 'salesPlanYear';
+export const SALES_PLAN_YEAR_COLLECTION_NAME = 'sales_plan_years';
+export const ACTIONS_PLANS_REPOSITORY_NAME = 'actionsPlan';
+export const ACTIONS_PLANS_COLLECTION_NAME = 'actions_plans';
+export const SALES_PLAN_REPOSITORY_NAME = 'salesPlan';
+export const SALES_PLAN_COLLECTION_NAME = 'sales_plans';
+export const SALES_PLAN_GROUP_REPOSITORY_NAME = 'salesPlanGroup';
+export const SALES_PLAN_GROUP_COLLECTION_NAME = 'sales_plan_groups';
+export const SALES_PLAN_SETTING_REPOSITORY_NAME = 'salesPlanSetting';
+export const SALES_PLAN_SETTING_COLLECTION_NAME = 'sales_plan_settings';
+export const SALES_PLAN_PRODUCT_REPOSITORY_NAME = 'salesPlanProduct';
+export const SALES_PLAN_PRODUCT_COLLECTION_NAME = 'sales_plan_products';
+export const SALES_PLAN_CONFIGURATION_REPOSITORY_NAME =
+  'salesPlanConfiguration';
+export const SALES_PLAN_CONFIGURATION_COLLECTION_NAME =
+  'sales_plan_configurations';
+export const SALES_PLAN_REVENUE_CONFIGURATION_REPOSITORY_NAME =
+  'salesPlanRevenueConfiguration';
+export const SALES_PLAN_REVENUE_CONFIGURATION_COLLECTION_NAME =
+  'sales_plan_revenue_configurations';
+export const SALES_PLAN_REVENUE_CONFIGURATION_DETAIL_REPOSITORY_NAME =
+  'salesPlanRevenueConfigurationDetail';
+export const SALES_PLAN_REVENUE_CONFIGURATION_DETAIL_COLLECTION_NAME =
+  'sales_plan_revenue_configuration_details';
+export const SALES_PLAN_VERSION_REPOSITORY_NAME = 'salesPlanVersion';
+export const SALES_PLAN_VERSION_COLLECTION_NAME = 'sales_plan_versions';
+export const SALES_PLAN_ALLOCATION_REPOSITORY_NAME = 'salesPlanAllocation';
+export const SALES_PLAN_ALLOCATION_COLLECTION_NAME = 'sales_plan_allocations';
+export const SALES_PLAN_SUBDIVISION_REPOSITORY_NAME = 'salesPlanSubdivision';
+export const SALES_PLAN_SUBDIVISION_COLLECTION_NAME = 'sales_plan_subdivisions';
+export const SALES_PLAN_SUBDIVISION_CONFIG_REPOSITORY_NAME =
+  'salesPlanSubdivisionConfig';
+
+// Sales Plan - collections view
+export const SALES_PLAN_DATA_STATISTICS_VIEWS_REPOSITORY_NAME =
+  'salesPlanDataStatisticsView';
+export const SALES_PLAN_DATA_STATISTICS_VIEWS_COLLECTION_NAME =
+  'sales_plan_data_statistics_views';
+export const SALES_PLAN_GROUPS_DATA_STATISTICS_VIEWS_REPOSITORY_NAME =
+  'salesPlanGroupsDataStatisticsView';
+export const SALES_PLAN_GROUPS_DATA_STATISTICS_VIEWS_COLLECTION_NAME =
+  'sales_plan_groups_data_statistics_views';
+
+// Purchase Plan
+export const PURCHASE_PLAN_REPOSITORY_NAME = 'purchasePlan';
+export const PURCHASE_PLAN_COLLECTION_NAME = 'purchase_plans';
+export const PURCHASE_PLAN_GROUP_REPOSITORY_NAME = 'purchasePlanGroup';
+export const PURCHASE_PLAN_GROUP_COLLECTION_NAME = 'purchase_plan_groups';
+
+// Department Mission
+export const DEPARTMENT_SEGMENT_REPOSITORY_NAME = 'departmentSegment';
+export const DEPARTMENT_SEGMENT_COLLECTION_NAME = 'department_segments';
+export const DEPARTMENT_TODO_REPOSITORY_NAME = 'departmentTodo';
+export const DEPARTMENT_TODO_COLLECTION_NAME = 'department_todos';
+export const DEPARTMENT_CHECKLIST_REPOSITORY_NAME = 'departmentChecklist';
+export const DEPARTMENT_CHECKLIST_COLLECTION_NAME = 'department_checklists';
+export const DEPARTMENT_REGULATION_REPOSITORY_NAME = 'departmentRegulation';
+export const DEPARTMENT_REGULATION_COLLECTION_NAME = 'department_regulations';
+export const DEPARTMENT_REGULATION_ITEM_REPOSITORY_NAME =
+  'departmentRegulationItem';
+export const DEPARTMENT_REGULATION_ITEM_COLLECTION_NAME =
+  'department_regulation_items';
+
+// FM02
+export const FM_STYLE_2_LIST_REPOSITORY_NAME = 'financialModelStyle2List';
+export const FM_STYLE_2_LIST_COLLECTION_NAME = 'financial_model_style_2_lists';
+export const NET_REVENUE_CONFIGURATION_REPOSITORY_NAME =
+  'netRevenueConfiguration';
+export const NET_REVENUE_CONFIGURATION_COLLECTION_NAME =
+  'net_revenue_configurations';
+export const FM_STYLE_2_CONFIGURATION_REPOSITORY_NAME =
+  'financialModelStyle2Configurations';
+export const FM_STYLE_2_CONFIGURATION_COLLECTION_NAME =
+  'financial_model_style_2_configurations';
+export const FM_STYLE_2_TARGET_REPOSITORY_NAME = 'financialModelStyle2Target';
+export const FM_STYLE_2_TARGET_COLLECTION_NAME =
+  'financial_model_style_2_target';
+
+// HumanResourcePlanning  - SalaryAndBenefit
+export const SALARY_BENEFIT_DEPARTMENT_REPOSITORY_NAME =
+  'salaryBenefitDepartment';
+export const SALARY_BENEFIT_DEPARTMENT_COLLECTION_NAME =
+  'salary_benefit_departments';
+export const SALARY_BENEFIT_PLAN_REPOSITORY_NAME = 'salaryBenefitPlan';
+export const SALARY_BENEFIT_PLAN_COLLECTION_NAME = 'salary_benefit_plans';
+export const SALARY_BENEFIT_PLAN_MONTH_REPOSITORY_NAME =
+  'salaryBenefitPlanMonth';
+export const SALARY_BENEFIT_PLAN_MONTH_COLLECTION_NAME =
+  'salary_benefit_plan_months';
+export const SALARY_BENEFIT_SPECIAL_COLUMN_REPOSITORY_NAME =
+  'salaryBenefitSpecialColumn';
+export const SALARY_BENEFIT_SPECIAL_COLUMN_COLLECTION_NAME =
+  'salary_benefit_special_columns';
+
+// Actual Sales
+export const ACTUAL_SALES_PRODUCT_REPOSITORY_NAME = 'actualSalesProduct';
+export const ACTUAL_SALES_PRODUCT_COLLECTION_NAME = 'actual_sales_products';
+
+export const ACTUAL_SALES_GROUP_REPOSITORY_NAME = 'actualSalesGroup';
+export const ACTUAL_SALES_GROUP_COLLECTION_NAME = 'actual_sales_groups';
+
+export const PLAN_QUANTITY_REPOSITORY_NAME = 'planQuantity';
+export const PLAN_QUANTITY_COLLECTION_NAME = 'plan_quantities';
+
+export const SALES_PLAN_YEAR_STATUS_REPOSITORY_NAME = 'salesPlanYearStatus';
+export const SALES_PLAN_YEAR_STATUS_COLLECTION_NAME = 'sales_plan_year_status';
+
+// FM01 Classification Mapping
+export const FINANCE_MODAL_CLASSIFICATION_MAPPING_REPOSITORY_NAME =
+  'financialModelClassificationMapping';
+export const FINANCE_MODAL_CLASSIFICATION_MAPPING_COLLECTION_NAME =
+  'financial_model_classification_mappings';
+
+// Order products
+export const ORDER_PRODUCTS_REPOSITORY_NAME = 'orderProduct';
+export const ORDER_PRODUCTS_COLLECTION_NAME = 'order_products';
+
+// SHARED SERVICE
+// Activity Log
+export const ACTIVITY_LOG_REPOSITORY_NAME = 'activityLog';
+export const ACTIVITY_LOG_COLLECTION_NAME = 'activity_logs';
+
+// Setting Common
+export const SETTING_COMMON_REPOSITORY_NAME = 'settingCommon';
+export const SETTING_COMMON_COLLECTION_NAME = 'setting_commons';
+
+// Setting Financial Year
+export const SETTING_FINANCIAL_YEAR_REPOSITORY_NAME = 'settingFinancialYear';
+export const SETTING_FINANCIAL_YEAR_COLLECTION_NAME = 'setting_financial_years';
+
+// Setting Access Token
+export const SETTING_ACCESS_TOKEN_REPOSITORY_NAME = 'settingAccessToken';
+export const SETTING_ACCESS_TOKEN_COLLECTION_NAME = 'setting_access_tokens';
+
+// Setting Ecommerce Platform
+export const SETTING_ECOMMERCE_PLATFORM_REPOSITORY_NAME =
+  'settingEcommercePlatform';
+export const SETTING_ECOMMERCE_PLATFORM_COLLECTION_NAME =
+  'setting_ecommerce_platforms';
+
+// Province
+export const PROVINCE_REPOSITORY_NAME = 'province';
+export const PROVINCE_COLLECTION_NAME = 'provinces';
+
+// District
+export const DISTRICT_REPOSITORY_NAME = 'district';
+export const DISTRICT_COLLECTION_NAME = 'districts';
+
+// PRODUCT SERVICE
+// in-out Inventories
+export const IN_OUT_INVENTORY_REPOSITORY_NAME = 'inOutInventory';
+export const IN_OUT_INVENTORY_COLLECTION_NAME = 'in_out_inventories';
+export const IN_OUT_PRODUCTS_REPOSITORY_NAME = 'inOutProduct';
+export const IN_OUT_PRODUCTS_COLLECTION_NAME = 'in_out_products';
+
+// Inventories
+export const INVENTORY_TRANSACTION_REPOSITORY_NAME = 'inventoryTransaction';
+export const INVENTORY_TRANSACTION_COLLECTION_NAME = 'inventory_transactions';
+export const INVENTORY_PRODUCTS_REPOSITORY_NAME = 'inventoryProduct';
+export const INVENTORY_PRODUCTS_COLLECTION_NAME = 'inventory_products';
+
+// Order Inventories for SOPO & another modules
+export const ORDER_INVENTORY_REPOSITORY_NAME = 'orderInventory';
+export const ORDER_INVENTORY_COLLECTION_NAME = 'order_inventories';
+
+// Inventories - Order Product Items
+export const ORDER_PRODUCT_ITEM_REPOSITORY_NAME = 'orderProductItem';
+export const ORDER_PRODUCT_ITEM_COLLECTION_NAME = 'order_product_items';
+
+// Warehouse Products
+export const WAREHOUSE_PRODUCT_REPOSITORY_NAME = 'warehouseProduct';
+export const WAREHOUSE_PRODUCT_COLLECTION_NAME = 'warehouse_products';
+
+// Warehouse Setting
+export const WAREHOUSE_SETTING_REPOSITORY_NAME = 'warehouseSetting';
+export const WAREHOUSE_SETTING_COLLECTION_NAME = 'warehouse_settings';
+
+// Group Labels
+export const GROUP_LABEL_REPOSITORY_NAME = 'productGroupLabel';
+export const GROUP_LABEL_COLLECTION_NAME = 'product_group_labels';
+
+// Product Items
+export const PRODUCT_ITEM_REPOSITORY_NAME = 'productItem';
+export const PRODUCT_ITEM_COLLECTION_NAME = 'product_items';
+
+// Product Details
+export const PRODUCT_DETAIL_REPOSITORY_NAME = 'productDetail';
+export const PRODUCT_DETAIL_COLLECTION_NAME = 'product_details';
+
+// Product Variants
+export const PRODUCT_VARIANT_REPOSITORY_NAME = 'productVariant';
+export const PRODUCT_VARIANT_COLLECTION_NAME = 'product_variants';
+
+// Product Combos
+export const PRODUCT_COMBO_REPOSITORY_NAME = 'productCombo';
+export const PRODUCT_COMBO_COLLECTION_NAME = 'product_combos';
+
+// Order products
+export const WAREHOUSE_REPOSITORY_NAME = 'warehouse';
+export const WAREHOUSE_COLLECTION_NAME = 'warehouses';
+
+// Accounting Settings
+export const ACCOUNTING_SETTING_REPOSITORY_NAME = 'accountingSetting';
+export const ACCOUNTING_SETTING_COLLECTION_NAME = 'accounting_settings';
+
+// Accounting Category
+export const ACCOUNTING_CATEGORY_REPOSITORY_NAME = 'accountingCategory';
+export const ACCOUNTING_CATEGORY_COLLECTION_NAME = 'accounting_categories';
+
+// Voucher Parameter
+export const VOUCHER_PARAMETER_REPOSITORY_NAME = 'voucherParameter';
+export const VOUCHER_PARAMETER_COLLECTION_NAME = 'voucher_parameters';
+
+// Product History
+export const PRODUCT_IMPORT_HISTORIES_REPOSITORY_NAME =
+  'productImportHistories';
+export const PRODUCT_IMPORT_HISTORIES_COLLECTION_NAME =
+  'product_import_histories';
+
+// Product Setting
+export const PRODUCT_SETTING_REPOSITORY_NAME = 'productSetting';
+export const PRODUCT_SETTING_COLLECTION_NAME = 'product_settings';
