e17d3f2b527732f52d5ef3af1a981f8fb89e83c6
thumv <maivanthu.cs@gmail.com>
2024-12-14 20:34:16 +0700

    chore: Improve type definitions in TypeScript

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,14 @@ import SERVICE_BROKER from "./service.constant.json";
 
 export default SERVICE_BROKER;
 
-export type ServiceBrokerConstantType = { [key: string]: any };
+export type ServiceBrokerConstantType = {
+  [K in keyof typeof SERVICE_BROKER]: (typeof SERVICE_BROKER)[K];
+};
 
-export { default as COLLECTIONS, CollectionsConstantType } from './collections.constant'
+export {
+  default as COLLECTIONS,
+  CollectionsConstantType,
+} from "./collections.constant";
 
 export const addLocalServices = (services: ServiceBrokerConstantType) => {
   Object.assign(SERVICE_BROKER, services);
